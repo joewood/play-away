5530cb587af332cb423bbe1d56eff3a425506bc4
Joe Wood <joewood@joes-mbp.myfiosgateway.com>
2020-04-11 11:43:09 -0400

    Retry Logic

--- src/room.tsx
+++ src/room.tsx
@@ -1,9 +1,25 @@
-import React, { FC, useCallback, useEffect, useState, useMemo } from "react";
+import React, { FC, useCallback, useEffect, useState, useMemo, useRef } from "react";
 import { usePeerState, useReceivePeerState } from "react-peer";
 import useDimensions from "react-use-dimensions";
 import "./App.css";
 import { MidiEvent, useActiveNotes, useMidiInputs, useMidiOutputs } from "./hooks";
 import { MidiSelect, PianoInput, StatusBar } from "./midi-components";
+import { connect } from "http2";
+
+function useTimeout(connected: boolean, seconds: number, cb: () => void) {
+    const ref = useRef<any>(null);
+    const timeout = useCallback(() => {
+        if (connected) return;
+        cb();
+    }, [connected, cb]);
+    useEffect(() => {
+        if (connected) return;
+        ref.current = setTimeout(timeout, seconds * 1000);
+        return () => {
+            if (ref.current) clearTimeout(ref.current);
+        };
+    }, [connected, seconds, timeout, ref]);
+}
 
 interface RoomProps {
     sendingName: string;
@@ -11,15 +27,29 @@ interface RoomProps {
 }
 
 const Teacher: FC<RoomProps> = ({ sendingName, receivingName }) => {
+    const [sendName, setSendName] = useState(receivingName);
+
     const [, sendData, , sendingConnections, errorSend] = usePeerState<MidiEvent>(
         { command: 0, note: 0, velocity: 0 },
-        { brokerId: sendingName }
+        { brokerId: sendName }
     );
-    const [receiveData, isReceiveConnected, errorReceive] = useReceivePeerState<MidiEvent>(receivingName);
+    const cb2 = useCallback(
+        () => setSendName(sendName === sendingName ? "SERVER" + Math.random() * 1000 : sendingName),
+        [sendingName, sendName]
+    );
+    useTimeout(sendingConnections.length > 0, 10, cb2);
+
+    const [recName, setRecName] = useState(receivingName);
+    const [receiveData, isReceiveConnected, errorReceive] = useReceivePeerState<MidiEvent>(recName);
+    const cb = useCallback(
+        () => setRecName(recName === receivingName ? "SERVER" + Math.random() * 1000 : receivingName),
+        [receivingName, recName]
+    );
+    useTimeout(isReceiveConnected, 10, cb);
 
     const [pianoData, setPianoData] = useState<MidiEvent | null>(null);
     const [midiInputData, setMidiInput] = useMidiInputs();
-    const [playMidiOutput, setMidiOutput] = useMidiOutputs();
+    const [, setMidiOutput] = useMidiOutputs();
     const [ref, { width }] = useDimensions();
     const onPianoInput = useCallback(
         (event: MidiEvent) => {
@@ -40,14 +70,14 @@ const Teacher: FC<RoomProps> = ({ sendingName, receivingName }) => {
                 <a href="/">Play Away - Playing "{sendingName}"</a>
             </header>
             <MidiSelect key="select" onInputSelect={setMidiInput} onOutputSelect={setMidiOutput} />
-            <div style={{ flex: "0 0 auto", pointerEvents: "none" }}>
+            <div style={{ flex: "0 0 auto", pointerEvents: "none", marginLeft: 30, marginRight: 30 }}>
                 <p>Remote: {isReceiveConnected ? "Connected" : "Not Connected"}</p>
-                <PianoInput disabled={!isReceiveConnected} width={width} activeNotes={remoteActiveNotes} />
+                <PianoInput disabled={!isReceiveConnected} width={width - 60} activeNotes={remoteActiveNotes} />
             </div>
-            <div style={{ flex: "0 0 auto" }}>
+            <div style={{ flex: "0 0 auto", marginLeft: 5, marginRight: 5 }}>
                 <p>You: {sendingConnections.length > 0 ? "Sending" : "Not Sending"} </p>
                 <PianoInput
-                    width={width}
+                    width={width - 10}
                     enableKeyboardShortcuts
                     onInput={onPianoInput}
                     activeNotes={midiActiveNotes}
@@ -55,7 +85,7 @@ const Teacher: FC<RoomProps> = ({ sendingName, receivingName }) => {
             </div>
             <StatusBar
                 error={errorSend || errorReceive}
-                session={`${sendingName}/${receivingName}`}
+                session={`${sendingName}/${recName}`}
                 connections={connectionNanes}
             />
         </div>
